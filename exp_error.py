fdbff25594507f2f7b814570a25d948970c3763a
Germax26 <45555658+Germax26@users.noreply.github.com>
2021-09-23 08:49:55 +1000

    Added line-specific error displaying
    An error message will only display the line that an error starts, and not any before it. I haven't added any functionality to display multiple lines if an error is on multiple lines, currently it will only display the first line. I am currently working on it.

--- exp_error.py
+++ exp_error.py
@@ -10,14 +10,37 @@ class Error(EnvironmentError):
         self.callbacks = []
     
     def display(self):
-        print(f"The {self.error_scope} encountered an unhandled {self.name}.")
+        line = 0
+        mod = 0
+        total = 0
+        
+        lines = self.expr.split("\n")
+        for i, j in enumerate(lines):
+            if self.span_left < total:
+                line = i
+                break
+            mod = total
+            total += len(j) + 1
+            self.expr = j
+        else:
+            line = len(lines)
+            self.expr=lines[-1]
+        
+        self.span_left -= mod
+
+        print(f"The {self.error_scope} encountered an unhandled {self.name} on line {line}.")
         for callback in self.callbacks:
             print("  " + callback[0])
             if callback[2] > 1:
                 print(f"  [previous line repeated {callback[2]-1} more times]")
         print(self.msg)
+
         print(self.expr)
+
         print(" " * self.span_left + "^" * self.span_right)
+
+        if self.span_right > len(self.expr) - self.span_left:
+            print("This error spans multiple lines")
     
     def in_scope(self, scope):
         self.error_scope = scope
