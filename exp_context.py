86554ed4cfb6a7e7ae02ad4791d0d597c6783370
Germax26 <45555658+Germax26@users.noreply.github.com>
2021-09-23 12:53:55 +1000

    Added the evaluate function to exp_context
    This function evaluates a node (by default self.right) within the context. All arguments to the self.int.interpret can be overriden by given them as arguments to the evaluate function.

--- exp_context.py
+++ exp_context.py
@@ -12,6 +12,20 @@ class Context:
         self.expr = source
         self.categories = categories
 
+    def evaluate(self, source=None, root=None, categories=None, variables=None) -> tuple:
+        """Evaluate a node in the given context, with options for overriding.
+
+        Args:
+            source (str, optional): Overridable source of evaluation. Defaults to None.
+            root (Node, optional): Overridable root node of evaluation. Defaults to None.
+            categories (list[Category], optional): Overridable categories of evaluation. Defaults to None.
+            variables (VARIABLE_LIST, optional): Overridable variables of evaluation. Defaults to None.
+
+        Returns:
+            tuple: (result, None) or (None, err)
+        """
+        return self.int.interpret(source or self.expr, root or self.right, categories or self.categories, variables or self.variables)
+
 exp_context = module(__name__)
 
 info = package_info(exp_context, "exp_context@v2 –– the built-in context package", [exp_info])
